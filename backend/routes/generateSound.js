f592c32f6e74f1bd603ef9f4385ca469183a70d7
Lucky Sharma <lucky1426sharma@gmail.com>
2025-10-09 22:34:17 +0530

    made controller for generateSound route

--- backend/routes/generateSound.js
+++ backend/routes/generateSound.js
@@ -1,33 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const {handleGetSound} = require("../controllers/generateSound");
 
-router.post("/", async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    if (!prompt) return res.status(400).json({ error: "Prompt is required" });
-
-    const response = await fetch("http://localhost:5002/generate", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt }),
-    });
-
-    if (!response.ok) {
-      const text = await response.text();
-      return res.status(response.status).send(text);
-    }
-
-    const audioBuffer = await response.arrayBuffer();
-    const audioUint8 = Buffer.from(audioBuffer);
-
-    res.set({
-      "Content-Type": "audio/wav",
-      "Content-Disposition": "attachment; filename=music.wav",
-    });
-    res.send(audioUint8);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.post("/", handleGetSound);
 
 module.exports = router;
