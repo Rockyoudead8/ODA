11f2e1ce10a4d75f3d95ff06e6f010f7e447c5d0
Lucky Sharma <lucky426sharma@gmail.com>
2026-03-25 09:51:47 +0530

    added auto scroll functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"
 
 import Check from './pages/Check'
@@ -10,77 +10,51 @@ import Footer from './components/Footer'
 import LoginPage from './pages/LoginPage'
 import Community from './pages/Community'
 import SignupPage from './pages/SignupPage'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
 import Explore from './pages/Explore'
 import ProtectedRoute from './components/ProtectedRoute'
 import ChatPage from './pages/ChatPage'
+import ScrollToTop from './components/ScrollToTop'
 
-function AppContent({ user }) {
+function AppContent() {
   const location = useLocation();
+  const hiddenFooterPaths = ["/Chat", "/Community"];
 
   return (
     <>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/Signup' element={<SignupPage />} />
+        <Route path='/Hero' element={<Hero />} />
 
         <Route path='/Check' element={<ProtectedRoute><Check /></ProtectedRoute>} />
         <Route path='/Admin' element={<ProtectedRoute><Admin /></ProtectedRoute>} />
-        
-        <Route path='/Hero' element={<Hero />} />
 
         <Route path='/Community' element={
-          <ProtectedRoute>
-            <Community />
-          </ProtectedRoute>
+          <ProtectedRoute><Community /></ProtectedRoute>
         } />
 
         <Route path='/Chat' element={
-          <ProtectedRoute>
-            <ChatPage user={user} />
-          </ProtectedRoute>
+          <ProtectedRoute><ChatPage /></ProtectedRoute>
         } />
 
         <Route path='/Specific/:id' element={
-          <ProtectedRoute>
-            <Specific />
-          </ProtectedRoute>
+          <ProtectedRoute><Specific /></ProtectedRoute>
         } />
       </Routes>
 
-      {/* 🔥 HIDE FOOTER ON CHAT PAGE */}
-      {location.pathname !== "/Chat"  && location.pathname !== "/Community" && <Footer />}
+      {!hiddenFooterPaths.includes(location.pathname) && <Footer />}
     </>
   );
 }
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/auth/status", {
-      credentials: "include",
-    })
-      .then(res => res.json())
-      .then(data => {
-        let actualUser = data.user || data;
-
-        if (actualUser?.id && !actualUser._id) {
-          actualUser._id = actualUser.id;
-        }
-
-        if (!actualUser?._id) return;
-
-        setUser(actualUser);
-      })
-      .catch(() => setUser(null));
-  }, []);
-
   return (
     <BrowserRouter>
-      <AppContent user={user} />
+      <AppContent />
     </BrowserRouter>
   );
 }
