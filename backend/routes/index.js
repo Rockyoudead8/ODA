344017f8e628a77acef797e80856103b45a03fa7
Lucky Sharma <lucky426sharma@gmail.com>
2026-03-25 23:47:21 +0530

    depolyable changes 2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ const category = require ("../models/category");
 const comment = require ("../models/comment");
 const listings = require ("../models/listings");
 const quiz = require ("../models/quiz");
-const quizResult = require ("../models/QuizResult");
+const quizResult = require ("../models/quizResult");
 
 
 router.get('/', function(req, res, next) {
