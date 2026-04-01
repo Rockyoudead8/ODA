70948ebc42faacd4ea458f368afe0335d31bb4d3
Lucky Sharma <lucky426sharma@gmail.com>
2026-04-01 23:29:10 +0530

    trying to fix images in messages

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ const userModel = require("./models/users");
 const cors = require('cors');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const jwt_strategy = require("./config/passport");
-const {app, server} = require("./config/socket");
+const { app, server } = require("./config/socket");
 
 app.set('trust proxy', 1); // Required on Render — sits behind a reverse proxy
 
@@ -56,8 +56,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
